1b91571fbd5384bfc5aee3a8bdf4bc34e50d5cac
MarioGK <mariogk01@gmail.com>
2022-08-30 18:10:03 -0300

    fixing paths

--- src/cv.PdfGenerator/Program.cs
+++ src/cv.PdfGenerator/Program.cs
@@ -1,22 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// code in your main method
-
-using System.Text.Json;
+﻿using System.Text.Json;
 using cv.Data;
 using QuestPDF.Fluent;
 using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 using QuestPDF.Previewer;
 
-var pdfDir = @"publish\wwwroot\pdfs\";
+const string pdfDir  = @"publish/wwwroot/pdfs/";
+const string dataDir = @"src/cv/wwwroot/data/";
 
-var languages = Directory.EnumerateFiles(@"src\cv\wwwroot\data\languages", "*.json")
+var languages = Directory.EnumerateFiles($"{dataDir}languages", " *.json")
                            .Select(File.ReadAllText)
                            .Select(json => JsonSerializer.Deserialize<LanguageData>(json)!)
                            .ToList();
 
-var skills      = JsonSerializer.Deserialize<List<SkillsData>>(File.ReadAllText(@"src\cv\wwwroot\data\skills.json"));
+var skills      = JsonSerializer.Deserialize<List<SkillsData>>(File.ReadAllText($"{dataDir}skills.json"));
 
 foreach(var language in languages)
 {
