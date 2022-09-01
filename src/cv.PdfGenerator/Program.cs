cbc2f4ca97798b36dbbbc10af4d7d7a6a427ebe5
MarioGK <mariogk01@gmail.com>
2022-08-31 22:45:05 -0300

    Layoutting the PDF better, making the print button work

--- src/cv.PdfGenerator/Program.cs
+++ src/cv.PdfGenerator/Program.cs
@@ -35,35 +35,51 @@ foreach (var language in languages)
         container.Page(page =>
         {
             page.Size(PageSizes.A4);
-            page.Margin(1, Unit.Centimetre);
+            //page.Margin(1, Unit.Centimetre);
             page.PageColor(Colors.White);
-            page.DefaultTextStyle(x => x.FontSize(16)
+            page.DefaultTextStyle(x => x.FontSize(12)
                                         .FontFamily("Roboto")
                                         .FontColor(Colors.Black));
 
             page.Header()
-                .Text(language.Language)
-                .Bold().FontSize(24).FontColor(Colors.Blue.Accent2);
+                .PaddingTop(1, Unit.Centimetre)
+                .PaddingHorizontal(1, Unit.Centimetre)
+                .Column(c =>
+                 {
+                     c.Item().Text(language.Language)
+                      .Bold().FontSize(24).FontColor(Colors.Blue.Accent2);
+
+                     c.Item().Text(language.Introduction);
+                     c.Item().PaddingBottom(5);
+                 });
 
 
             page.Content()
-                .PaddingVertical(1, Unit.Centimetre)
+                .PaddingHorizontal(1, Unit.Centimetre)
+                .PaddingBottom(10)
                 .Column(column =>
                  {
                      column.Spacing(10);
                      foreach (var exp in language.Experiences)
                      {
-                         column.Item().Text(exp.Title).SemiBold().FontColor(Colors.Blue.Accent1);
-                         column.Item().Text(exp.Description).FontSize(12);
+                         column.Item()
+                               .Text(exp.Title)
+                               .SemiBold()
+                               .FontSize(14)
+                               .FontColor(Colors.Blue.Accent1);
+                         column.Item().Text(exp.Description);
                      }
                  });
-
+            
             page.Footer()
-                .AlignCenter()
-                .Text(x =>
+                .DefaultTextStyle(text => text.FontSize(9).Light())
+                .AlignRight()
+                .AlignBottom()
+                .Row(row =>
                  {
-                     x.Span("Page ");
-                     x.CurrentPageNumber();
+                     row.AutoItem().Text($"*This PDF was automatically generated from ");
+                     row.AutoItem().Hyperlink("https://cv.mariogk.top/").Text("cv.mariogk.top").FontColor(Colors.Blue.Darken1);
+                     row.AutoItem().Text(" for a better experience please visit the website.");
                  });
         });
     });
--- src/cv/Data/LanguageData.cs
+++ src/cv/Data/LanguageData.cs
@@ -4,6 +4,8 @@ public class LanguageData
 {
     public string Language { get; set; } = null!;
 
+    public string Introduction { get; set; } = null!;
+
     public Dictionary<string,string> Translations { get; set; } = new();
 
     public List<ExperienceData> Experiences { get; set; } = new();
